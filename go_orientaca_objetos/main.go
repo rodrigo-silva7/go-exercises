097a5edea5782c2d5a18275bd59cae6b141a9823
Rodrigo Silva <rodrigo.silva7@yahoo.com.br>
2022-07-23 11:42:17 -0300

    feat: aula 3

--- go_orientaca_objetos/main.go
+++ go_orientaca_objetos/main.go
@@ -1,20 +1,53 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-   titular         string
-   numeroAgencia   int
-   numeroConta     int
-   saldo           float64
-} 
+import (
+   "fmt"
+   m "model"
+)
 
 func main() {
 
-   contaRodrigo := ContaCorrente{titular: "Rodrigo", numeroAgencia: 589, numeroConta: 123456, saldo: 125.50}; 
-   contaAline := ContaCorrente{"Aline", 590, 123491, 356.15};
-   
-   fmt.Println(contaRodrigo);
-   fmt.Println(contaAline);
+////   contaRodrigo := m.ContaCorrente{titular: "Rodrigo", numeroAgencia: 589, numeroConta: 123456, saldo: 125.50}; 
+   //contaAline := m.ContaCorrente{"Aline", 590, 123491, 356.15};
+   //
+   ////fmt.Println(contaRodrigo);
+   ////fmt.Println(contaAline);
+//
+   //var contaDaCris *m.ContaCorrente
+   //contaDaCris = new(m.ContaCorrente)
+   //contaDaCris.Titular = "Cris"
+   //contaDaCris.Saldo = 500
+   //contaDaCris.NumeroAgencia = 591
+   //contaDaCris.NumeroConta = 123492
+//
+   //contaAline.Depositar(200);
+   //contaRodrigo.Depositar(300);
+//
+   //fmt.Println(*contaDaCris);
+   //fmt.Println(contaDaCris.Sacar(300));
+   //fmt.Println(*contaDaCris);
+//
+   //statusString, valor := contaDaCris.Depositar(500);
+   //
+   //fmt.Println("status:", statusString);
+   //fmt.Println("Novo saldo é:", valor);
+   //
+   //fmt.Println(*contaDaCris);
+   //fmt.Println(contaAline);
+//
+   //contaAline.Transferir(200, contaDaCris);
+   //
+   //fmt.Println(*contaDaCris);
+   //fmt.Println(contaAline);
+//
+   //contaDaCris.Transferir(200, &contaAline);
+   //
+   //fmt.Println(*contaDaCris);
+   //fmt.Println(contaAline);
+
+   conta1 := m.ContaCorrente{Titular: "André", Saldo: 300};
+   conta2 := m.ContaCorrente{Titular: "Júlia", Saldo: 150};
+
+   status := conta1.
 
 }
